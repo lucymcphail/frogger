edb9fcfbfa61d115d7e9509596b313fcea6ba4ed
Lucy McPhail <lucy.mcphail.p@gmail.com>
2017-06-23 20:10:31 +0100

    Initial game commit

---
+++ main/obstacles.js
@@ -0,0 +1,49 @@
+function obstacleLog(row, direction) {
+    this.speed = direction;
+    this.width = 200;
+    this.height = 60;
+    this.x = 0 - this.width;
+    this.y = row * scl + 20;
+
+    if (direction === 1) {
+        this.speed = 1;
+        this.x = 0 - this.width;
+    } else {
+        this.speed = -1;
+        this.x = width;
+    }
+
+    this.show = function() {
+        fill(79, 37, 0);
+        rect(this.x, this.y, this.width, this.height);
+    }
+
+    this.update = function() {
+        this.x += this.speed;
+    }
+}
+
+function obstacleCar(row, direction) {
+    this.speed = direction;
+    this.width = 100;
+    this.height = 60;
+    this.x = 0 - this.width;
+    this.y = row * scl + 20;
+
+    if (direction === 1) {
+        this.speed = 1;
+        this.x = 0 - this.width;
+    } else {
+        this.speed = -1;
+        this.x = width;
+    }
+
+    this.show = function() {
+        fill(200, 20, 20);
+        rect(this.x, this.y, this.width, this.height);
+    }
+
+    this.update = function() {
+        this.x += this.speed;
+    }
+}
