9780884ad56600880ecc7d53af3616c90e839cdb
Lucy McPhail <lucy.mcphail.p@gmail.com>
2017-06-23 21:36:28 +0100

    Added obstacle respawning

--- main/obstacles.js
+++ main/obstacles.js
@@ -1,53 +1,69 @@
 function obstacleLog(row, direction) {
+    this.spawn = function () {
+        if (direction === 1) {
+            this.speed = 1;
+            this.x = 0 - this.width;
+        } else {
+            this.speed = -1;
+            this.x = width;
+        }
+    }
+
     this.width = 200;
     this.height = 60;
     this.x = 0 - this.width;
     this.y = row * scl + 20;
 
-    if (direction === 1) {
-        this.speed = 1;
-        this.x = 0 - this.width;
-    } else {
-        this.speed = -1;
-        this.x = width;
-    }
+    this.spawn();
 
-    this.show = function() {
+    this.show = function () {
         fill(79, 37, 0);
         rect(this.x, this.y, this.width, this.height);
     }
 
-    this.update = function() {
+    this.update = function () {
         this.x += this.speed;
+
+        if (this.x >= width || this.x < 0 - this.width) {
+            this.spawn();
+        }
     }
 }
 
 function obstacleCar(row, direction) {
+    this.spawn = function () {
+        if (direction === 1) {
+            this.speed = 1;
+            this.x = 0 - this.width;
+        } else {
+            this.speed = -1;
+            this.x = width;
+        }
+    }
+
     this.width = 100;
     this.height = 60;
     this.x = 0 - this.width;
     this.y = row * scl + 20;
 
-    if (direction === 1) {
-        this.speed = 1;
-        this.x = 0 - this.width;
-    } else {
-        this.speed = -1;
-        this.x = width;
-    }
+    this.spawn();
 
-    this.show = function() {
+    this.show = function () {
         fill(200, 20, 20);
         rect(this.x, this.y, this.width, this.height);
     }
 
-    this.update = function() {
+    this.update = function () {
         this.x += this.speed;
+
+        if (this.x >= width || this.x < 0 - this.width) {
+            this.spawn();
+        }
     }
 
-    this.hitFrog = function() {
-        if (this.x + this.width >= frogger.x - frogger.radius && this.x <= frogger.x + frogger.radius) {
-            if (this.y + this.height >= frogger.y - frogger.radius && this.y <= frogger.y + frogger.radius) {
+    this.hitFrog = function () {
+        if (this.x + this.width >= frogger.x && this.x <= frogger.x) {
+            if (this.y + this.height >= frogger.y && this.y <= frogger.y) {
                 frogger.die();
             }
         }
